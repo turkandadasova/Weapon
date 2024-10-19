69125a776d8c4950514c9c691e259431ae086c1d
Your Name <you@example.com>
2024-10-19 17:43:46 +0400

    first commit

---
+++ Weaponn.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp33
+{
+    internal class Program
+    {
+        //tam qesey islemir,max bu qeder yaza bilirem,bunla idare eliyin:(
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(10,23,false);
+            
+           
+
+            bool f = false;
+            string choice;
+            do
+            {
+                Console.WriteLine("0. Informasiya alin  " +
+                    "1. Ates acin  " +
+                    "2. Tam dolmaga lazim olan gulle sayini goster  " +
+                    "3. Daragi yeniden doldur  " +
+                    "4. Atis modunu deyis  " +
+                    "5. Proqrami dayandır.");
+
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        
+                        weapon.ShowFullInfo();
+                        break;
+
+                    case "1":
+                        weapon.Shoot();
+                        break;
+
+                    case "2":
+                        weapon.GetRemainBulletCount();
+                        break;
+
+                     case "3":
+                        weapon.Reload();
+                        break;
+
+                     case "4":
+                        weapon.ChangeFireMode();
+                        break;
+
+                     case "5":
+                        f = true;
+                        break;
+
+                }
+            }
+            while (!f);
+
+
+
+
+
+        }
+    }
+}
